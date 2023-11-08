5f9af59d02daf4bbb977f85c7b52a76037c64fe4
Moises Munaldi <ork.mgm@gmail.com>
2023-11-08 17:59:45 +0000

    Firt push

---
+++ template.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on a day with no parking space
+
+
+Student Name: ???
+
+Student ID: ???
+
+Cohort: evSD3
+
+"""
+
+
+
+def task1():
+    
+    """
+    your code
+    """
+
+
+
+def task2():
+    
+    """
+    your code
+    """
+    
+    
+    
+    
+def task3():
+    
+    """
+    your code
+    """
+    
+    
+    
+    
+    
+def task4():
+    
+    """
+    your code
+    """
+    
+    
+    
+    
+def task5():
+    
+    """
+    your code
+    """
+    
+    
+    
+    
+    
+def task6():
+    
+    """
+    your code
+    """
